d528ffc43c13a210b11cf1a64877fa31cbe20fc7
ivanmirable <ivankrivonogov1823@mail.ru>
2024-07-05 16:23:02 +0400

    refactor: Add error message control

---
+++ ItLabs/UserControls/ErrorMessage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItLabs.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorMessage.xaml
+    /// </summary>
+    public partial class ErrorMessage : UserControl
+    {
+
+        public ErrorMessage()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ErrorMessage), new PropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
